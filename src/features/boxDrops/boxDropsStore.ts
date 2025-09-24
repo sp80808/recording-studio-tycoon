6b74d14dbf9bf7bc0c4905ba2e422301cccc3272
Harry Speight <211996837+sp80808@users.noreply.github.com>
2025-09-24 00:24:16 +0000

    refactor: Update import statements for zustand to use named imports

--- src/features/boxDrops/boxDropsStore.ts
+++ src/features/boxDrops/boxDropsStore.ts
@@ -1,4 +1,4 @@
-import create from 'zustand'
+import { create } from 'zustand'
 import { generateBoxLoot, EquipmentItem, Era } from './lootGenerator'
 
 type BoxDropsState = {
--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -1,12 +1,13 @@
-import create from 'zustand'
+import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createProductionQueueSlice, ProductionQueueSlice } from './productionQueueSlice'
 import { createQuickAssignSlice, QuickAssignSlice } from './quickAssignSlice'
 
 type RootState = ProductionQueueSlice & QuickAssignSlice & {}
 
+// Compose slices by calling each slice factory with the (set, get, api) arguments
 export const useRootStore = create<RootState>()(
-  devtools((...a: any[]) => ({ ...createProductionQueueSlice(...a), ...createQuickAssignSlice(...a) }))
+  devtools((set, get, api) => ({ ...createProductionQueueSlice(set, get, api), ...createQuickAssignSlice(set, get, api) }))
 )
 
 export const useProductionQueueStore = (selector: any) => useRootStore(selector)
